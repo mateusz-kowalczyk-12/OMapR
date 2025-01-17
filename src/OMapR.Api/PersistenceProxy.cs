8be50b3cf5c2c90b2a57b52d5a22b58379e07079
Mateusz Kowalczyk <116964693+mateusz-kowalczyk-12@users.noreply.github.com>
2025-01-17 21:50:58 +0100

    [OM-6] Enable adding OMapR to a DI container (#2)

---
+++ src/OMapR.Api/PersistenceProxy.cs
@@ -0,0 +1,21 @@
+﻿using OMapR.Application;
+
+namespace OMapR.Api;
+
+
+public class PersistenceProxy : IPersistenceProxy
+{
+    private readonly ICore _core;
+
+    
+    public PersistenceProxy(ICore core)
+    {
+        _core = core;
+    }
+
+
+    public void ConnectToDb()
+    {
+        _core.ConnectToDb();
+    }
+}
---
+++ src/OMapR.Application/Options/OMapROptions.cs
@@ -0,0 +1,16 @@
+﻿using OMapR.Application.Common.Enums;
+
+namespace OMapR.Application.Options;
+
+
+public class OMapROptions
+{
+    internal string ConnectionString { get; set; } = string.Empty;
+    internal DbProvider DbProvider { get; set; }
+
+    public void UseSqlServer(string connectionString)
+    {
+        ConnectionString = connectionString;
+        DbProvider = DbProvider.SqlServer;
+    }
+}
